c1a89d754c9728f8a2bc140520ef1546664bcdc0
MVONDO Yannick <mvondoyannick@gmail.com>
2023-02-07 13:22:25 +0100

    adding staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,6 +7,8 @@
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
 
+server '116.202.91.85', user: 'essiolus143', roles: %w{app db web}
+
 
 
 # role-based syntax
