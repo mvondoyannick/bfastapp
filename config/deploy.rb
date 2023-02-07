9974c369934fde0fd1765afa4b3f0df63cac34f1
MVONDO Yannick <mvondoyannick@gmail.com>
2023-02-07 09:40:31 +0100

    adding cap deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.17.1"
 
 set :application, "bfast"
-set :repo_url, "git@example.com:mvondoyannick/bfast.git"
+set :repo_url, "git@github.com:mvondoyannick/bfastapp.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
